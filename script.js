33fadd92e5e588fb8e421726e8b860bfd5372151
Rafael Fernando <rafaelribeirosouza86@gmail.com>
2019-02-17 18:10:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,23 @@ let workbook = XLSX.readFile(__dirname+'/Master.xlsx');
 /* pegamos as folhas*/
 let sheet_name_list = workbook.SheetNames;
 /* pegamos a primeira folha*/
-let xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
-/* convertemos os objetos em json */
-let json = JSON.stringify(xlData);
+let firstdata = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+/* pegamos a segunda folha*/
+let seconddata = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[1]]);
 
-console.log(">>"+json);
+/* Mostramos os resultados da primeira folha */
+for (key in firstdata) {
+    console.log(key, firstdata[key]);
+    for (x in firstdata[key]) {
+        console.log(x,firstdata[key][x]);
+    }
+}
 
-/* Mostramos os resultados */
-for (key in xlData) {
-    console.log(key, xlData[key]);
-    for (x in xlData[key]) {
-        console.log(x,xlData[key][x]);
+/* Mostramos os resultados da segunda folha */
+for (key in seconddata) {
+    console.log(key, seconddata[key]);
+    for (x in seconddata[key]) {
+        console.log(x,seconddata[key][x]);
     }
 }
 
