1c9d1d531a6c13449fc8480ab2269974374bdd84
Rafael Fernando <rafaelribeirosouza86@gmail.com>
2019-02-17 17:49:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-let XLSX = require('xlsx')
+/* carregamos o module */
+let XLSX = require('xlsx');
+/* carregamos o arquivo xlsx */
 let workbook = XLSX.readFile(__dirname+'/Master.xlsx');
+/* pegamos as folhas*/
 let sheet_name_list = workbook.SheetNames;
+/* pegamos a primeira folha*/
 let xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+/* convertemos os objetos em json */
 let json = JSON.stringify(xlData);
 
 console.log(">>"+json);
 
+/* Mostramos os resultados */
 for (key in xlData) {
     console.log(key, xlData[key]);
     for (x in xlData[key]) {
